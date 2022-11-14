6036e16ea194d1129a7713adcc8bb89abd7facfc
SH-NicholasPage <npage@southhills.edu>
2022-11-14 00:28:43 -0500

    Initial commit

---
+++ War/War/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+using System;
+
+namespace War
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
